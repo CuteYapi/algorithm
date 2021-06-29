64b52d2c62fd234a4af9c109e511414f3eee275b
CuteYapi <yapi42@gmail.com>
2021-06-29 18:47:21 +0900

    /*  * Algorithm Day 3  * UglyNumber  * https://leetcode.com/problems/ugly-number/  *  * 44 ms, faster than 44.22% of C#  */

--- AlgorithmStudy/AlgorithmStudy/Program.cs
+++ AlgorithmStudy/AlgorithmStudy/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MergeTwoSortedLists;
 
 namespace AlgorithmStudy
 {
@@ -11,22 +6,10 @@ namespace AlgorithmStudy
     {
         static void Main(string[] args)
         {
-            ListNode l1 = new ListNode(1);
-           // l1.next = new ListNode(2);
-            //l1.next.next = new ListNode(5);
+            UglyNumber.Solution solution = new UglyNumber.Solution();
+            var quizAnswer = solution.IsUgly(0);
 
-            ListNode l2 = new ListNode(2);
-            //l2.next = new ListNode(3);
-            //l2.next.next = new ListNode(4);
-
-            MergeTwoSortedLists.Solution solution = new MergeTwoSortedLists.Solution();
-            var quizAnswer = solution.MergeTwoLists(l1, l2);
-
-            while (!(quizAnswer == null))
-            {
-                Console.WriteLine(quizAnswer.val);
-                quizAnswer = quizAnswer.next;
-            }
+            Console.WriteLine(quizAnswer);
         }
     }
-} 
+}

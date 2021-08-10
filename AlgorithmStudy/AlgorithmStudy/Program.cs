9ab607e59e35bb6425b35728636ff48fe9f44c39
CuteYapi <yapi42@gmail.com>
2021-08-11 00:09:47 +0900

    20210811 HappyNumber with Cpp

--- AlgorithmStudy/AlgorithmStudy/Program.cs
+++ AlgorithmStudy/AlgorithmStudy/Program.cs
@@ -6,12 +6,12 @@ namespace AlgorithmStudy
     {
         static void Main(string[] args)
         {
-            string num1 = "9009";
-            string num2 = "9203";
+            //    string num1 = "9009";
+            //    string num2 = "9203";
 
-            AddStrings.Solution solution = new AddStrings.Solution();
-            
-            Console.WriteLine(solution.AddStrings(num1, num2).ToCharArray());
+            //    AddStrings.Solution solution = new AddStrings.Solution();
+
+            //    Console.WriteLine(solution.AddStrings(num1, num2).ToCharArray());
         }
     }
 }
